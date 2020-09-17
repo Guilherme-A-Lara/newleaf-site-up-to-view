cebaf9ed02f8e63b9ed9ffbebc1aa1e86fd700e4
Guilherme Lara <54341844+Guilherme-A-Lara@users.noreply.github.com>
2020-09-17 18:30:16 -0300

    First Up
    Subindo arquivos para preview em smarts

---
+++ js/typed.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#typed', {
+    strings: [
+        'WebSites',
+        'Landing Pages',
+        'Front-End'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
